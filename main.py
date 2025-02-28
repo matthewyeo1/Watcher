de16bd49f73cc72e833c22df0c34a42a6ac102d5
Matthew <e1155311@u.nus.edu>
2025-03-01 03:49:17 +0800

    Prints final output (less predicted price)

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from transformer import train_and_predict
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 if __name__ == "__main__":
     train_and_predict()
