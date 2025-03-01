ebdac89d946ec7342a30790250e798e623cfa1fc
Desai Wang <dw584@cornell.edu>
2025-03-01 21:16:16 +0800

    stock list next to pricing

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -10,16 +10,16 @@ export default function Home() {
       <h1 className="text-8xl font-bold text-blue-700 tracking-wider mb-6 pb-4 border-b-2 border-blue-900">
         Watcher
       </h1>
-      <main className="max-w-7xl mx-auto">
-        <StockChart />
-        <PredictionPrice />
-        <NewsArticleList />
-        <NewsArticle
-          source="Reuters"
-          title="Apple to buy $100M of Bitcoin"
-          time="2021-02-28"
-          sentiment={0.5}
-        />
+      <main className="max-w-7xl mx-auto h-[calc(100vh-200px)]">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 h-full">
+          <div className="lg:col-span-2 space-y-8">
+            <StockChart />
+            <PredictionPrice />
+          </div>
+          <div className="lg:col-span-1 overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:bg-slate-700 [&::-webkit-scrollbar-thumb]:rounded-full">
+            <NewsArticleList />
+          </div>
+        </div>
       </main>
     </div>
   );
